d6d7068283bebe4c17cf3f69f78db4baf4948295
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-30 11:09:43 -0300

    feat: pass function handleNextMovies to the button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { Lightning } from "@phosphor-icons/react";
 import logo from '../assets/logo.svg'
 
-export function Header() {
+interface HeaderProps {
+  handleNextMovies: () => void
+}
+
+export function Header({handleNextMovies}: HeaderProps) {
   return (
     <header className="flex items-center justify-between">
       <img src={logo} alt="Logo" />
-      <button className="max-w-[220px] w-full h-12 flex items-center justify-center gap-2 bg-buttonGradient text-white rounded">
+      <button className="max-w-[220px] w-full h-12 flex items-center justify-center gap-2 bg-buttonGradient text-white rounded" onClick={handleNextMovies}>
         <span>Nova recomendação</span>
         <div className="w-8 h-8 bg-white/20 flex justify-center items-center rounded-full">
           <Lightning size={18} />

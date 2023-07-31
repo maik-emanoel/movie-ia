8fee5d767ed4c56b88e218940c98d068fbb2b3df
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-31 10:47:12 -0300

    add max-screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,22 @@ export default {
             transform: 'scale(1)',
           },
         },
+      },
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '790px'},
+        // => @media (max-width: 790px) { ... }
+  
+        'sm': {'max': '530px'},
+        // => @media (max-width: 530px) { ... }
       }
     },
   },
